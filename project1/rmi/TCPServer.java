897bb0739ff66786b16dce3297cc25c0c08d0fd5
ShuminW <shumin.wang94@gmail.com>
2018-02-02 19:44:39 -0800

    update server code

--- project1/rmi/TCPServer.java
+++ project1/rmi/TCPServer.java
@@ -1,82 +1,110 @@
 package rmi;
+
 import java.io.*;
 import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
 
-public class TCPServer<T> {
+public class TCPServer<T>{
     public static final int PORT = 12345;
     Class<T> c;
     T server;
-    InetSocketAddress address;
-    boolean stop = false;
+    InetSocketAddress address = null;
+    ServerSocket serverSocket = null;
+    Skeleton skeleton = null;
+    ArrayList<OPThread> threads = new ArrayList<>();
+    boolean started = false;
 
-    public TCPServer(Class<T> c, T server, InetSocketAddress address) {
-        this. c = c;
+    HandlerThread handlerThread  = null;
+
+    public TCPServer(Class<T> c, T server, InetSocketAddress address, Skeleton skeleton) {
+        this.c = c;
         this.server = server;
         this.address = address;
-        init();
+        this.skeleton = skeleton;
 
     }
 
-    public void init() {
-        try {
+    public void stopThread() {
+        if(handlerThread != null) {
+            handlerThread.stopThread();
+            try {
+                handlerThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
-            ServerSocket serverSocket = new ServerSocket();
-            if(serverSocket != null) {
+    }
+
+    public void start() {
+
+        try {
+            serverSocket = new ServerSocket();
+            if(address != null) {
                 serverSocket.bind(address);
+                skeleton.address = address;
             }
             else {
                 serverSocket.bind(new InetSocketAddress(PORT));
-            }
+                skeleton.address = new InetSocketAddress(serverSocket.getInetAddress(),
+                        serverSocket.getLocalPort());
 
-            while (!stop) {
-                Socket client = serverSocket.accept();
-                new HandlerThread(client);
             }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+            handlerThread = new HandlerThread(serverSocket);
+            handlerThread.start();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
 
-    private class HandlerThread implements Runnable {
-        private Socket socket;
-        ObjectOutputStream output;
-        ObjectInputStream input;
-        public HandlerThread(Socket client) {
-            socket = client;
-            new Thread(this).start();
-        }
 
+    private class OPThread extends Thread {
+
+        private Socket socket = null;
+
+        public OPThread(Socket socket) {
+            this.socket = socket;
+
+        }
         public void run() {
+            ObjectInputStream input = null;
+            ObjectOutputStream output = null;
+
             try {
+
                 output = new ObjectOutputStream(socket.getOutputStream());
 
                 output.flush();
 
-                input = new ObjectInputStream (socket.getInputStream());
+                input = new ObjectInputStream(socket.getInputStream());
 
                 Method method = (Method)input.readObject();
 
                 Object[] args = (Object[])input.readObject();
 
+
                 Object object = method.invoke(server, args);
 
                 output.writeObject(object);
 
+            }
+            catch (Exception e) {
+                e.printStackTrace();
 
-
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            } finally {
-                if (socket != null) {
+            }
+            finally {
+                if(socket!=null) {
                     try {
                         socket.close();
-                    } catch (Exception e) {
-                        socket = null;
-                        System.out.println(e.getMessage());
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
                 }
                 if(input != null) {
@@ -93,8 +121,76 @@ public class TCPServer<T> {
                         e.printStackTrace();
                     }
                 }
-
             }
+
+
         }
     }
-}  
+
+    private class HandlerThread extends Thread {
+
+        private ServerSocket serverSocket = null;
+
+        private volatile boolean stop = false;
+
+        public HandlerThread(ServerSocket serverSocket) {
+            this.serverSocket = serverSocket;
+        }
+
+        public void stopThread() {
+            stop = true;
+            if (serverSocket != null) {
+                try {
+                    serverSocket.close();
+                } catch (Exception e) {
+                    serverSocket = null;
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+        public void run() {
+            try {
+                while(!stop) {
+                    Socket socket = serverSocket.accept();
+
+                    OPThread opThread = new OPThread(socket);
+
+                    threads.add(opThread);
+
+                    opThread.start();
+                }
+
+                try {
+                    for(OPThread t: threads) {
+                        t.join();
+                    }
+
+                }
+                catch(InterruptedException ep) {
+                    ep.printStackTrace();
+
+                }
+
+            } catch(SocketException e) {
+                try {
+                    if(serverSocket.isClosed()){
+                        for(OPThread t: threads) {
+                            t.join();
+                        }
+                    }
+
+                }
+                catch(InterruptedException ep) {
+                    ep.printStackTrace();
+
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
